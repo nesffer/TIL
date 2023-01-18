880d30a6b12b8a34517bb9cd0b351569ee2fe405
Nesffer <nesffer.jimin@gmail.com>
2023-01-18 10:56:18 +0900

    useEffect 연습

--- nomadcoders/react-for-beginners/src/App.js
+++ nomadcoders/react-for-beginners/src/App.js
@@ -1,11 +1,21 @@
-import Button from './Button'
-import style from './App.module.css'
+import { useState, useEffect } from 'react'
 
 function App() {
+  console.log('Call API')
+  const [counter, setCounter] = useState(0)
+  const [search, setSearch] = useState('')
+  const onClick = () => setCounter((prev) => prev + 1)
+  const onChange = (event) => setSearch(event.target.value)
+  useEffect(() => {
+    if (search && search.length > 5) {
+      console.log('Searching: ', search)
+    }
+  }, [search])
   return (
     <div>
-      <h1 className={style.title}>Hello World!</h1>
-      <Button text={'Apply'} />
+      <input type="text" placeholder="여기에 검색" onChange={onChange} value={search}></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   )
 }
