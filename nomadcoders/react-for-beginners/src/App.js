5a836f8fc78c8455a6def73e32eb4a4e5f9b1e86
Nesffer <nesffer.jimin@gmail.com>
2023-01-30 13:56:19 +0900

    To-Do 기본 작성

--- nomadcoders/react-for-beginners/src/App.js
+++ nomadcoders/react-for-beginners/src/App.js
@@ -1,21 +1,30 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 function App() {
-  console.log('Call API')
-  const [counter, setCounter] = useState(0)
-  const [search, setSearch] = useState('')
-  const onClick = () => setCounter((prev) => prev + 1)
-  const onChange = (event) => setSearch(event.target.value)
-  useEffect(() => {
-    if (search && search.length > 5) {
-      console.log('Searching: ', search)
-    }
-  }, [search])
+  const [toDo, setToDo] = useState('')
+  const [toDos, setToDos] = useState([])
+
+  const onChange = (event) => setToDo(event.target.value)
+  const onSubmit = (event) => {
+    event.preventDefault()
+    if (toDo === '') return
+    setToDos((currentToDos) => [...currentToDos, toDo])
+    setToDo('')
+  }
+
   return (
     <div>
-      <input type="text" placeholder="여기에 검색" onChange={onChange} value={search}></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      <h1>My To Do ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder="할 일을 작성해주세요." value={toDo} onChange={onChange}></input>
+        <button>할 일 추가</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((toDo, index) => (
+          <li key={index}>{toDo}</li>
+        ))}
+      </ul>
     </div>
   )
 }
